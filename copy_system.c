06518216299dde79b406c2280251bf2cb5bdd215
vaois_ping <851149343@qq.com>
2016-04-26 22:23:54 +0800

    add head file stdio.h in copy_system.c

--- copy_system.c
+++ copy_system.c
@@ -3,12 +3,18 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc,char *argv[])
 {
     char c;
     int in, out;
-    in = open("file.in",O_RDONLY);
-    out = open("file.out",O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+    if (argc < 3)
+    {
+	printf("Usage:copy filename newname\n");
+
+    }
+    
+    in = open(argv[1],O_RDONLY);
+    out = open(argv[2],O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
     while(read(in,&c,1)==1)
           write(out,&c,1);
     exit(0);
