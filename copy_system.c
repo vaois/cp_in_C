e93069274882f494953fa515290521bdff628f2b
zhiping <851149343@qq.com>
2016-05-04 13:47:27 +0800

    增加找不到文件的错误提示

--- copy_system.c
+++ copy_system.c
@@ -2,6 +2,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
 
 int main(int argc,char *argv[])
 {
@@ -14,6 +17,10 @@ int main(int argc,char *argv[])
     }
     
     in = open(argv[1],O_RDONLY);
+    if(in = -1)
+    {
+    	printf("\"%s\"  %s\n",argv[1],strerror(errno));
+    }
     out = open(argv[2],O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
     while(read(in,&c,1)==1)
           write(out,&c,1);
